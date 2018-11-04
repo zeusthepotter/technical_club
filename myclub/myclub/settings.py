ebaab03503bb03efbc470c85ec309a9db49e33a6
zeusthepotter <zeusthepotter@gmail.com>
2018-11-04 22:40:01 +0530

    sb admin

--- myclub/myclub/settings.py
+++ myclub/myclub/settings.py
@@ -121,6 +121,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-LOGIN_REDIRECT_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/portal/profile/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
